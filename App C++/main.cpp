eb0e6c777de94bca8a0958964e796788b5b8b0cf
Bruno Campagnol <bruno_unky@hotmail.com>
2024-05-08 21:23:30 -0300

    Criação parcial do menu inicial

--- App C++/main.cpp
+++ App C++/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <stdlib.h>
+#include <locale>
+#include <cstring>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
 	
-	cout >> "OI" >> endl;
+	setlocale (LC_ALL, "Portuguese");
+	using namespace std;
+	
+	cout << "___________ Turma do Cafe - Fatec Araras ___________" << endl;
+	
+	cout << "Digite a opção desejada: " << endl;
+	cout << "1 - Inserir um novo contribuente" << endl;
+	cout << "2 - Editar dados de um contribuente já cadastrado" << endl;
+	
 	
 	return 0;
 	
