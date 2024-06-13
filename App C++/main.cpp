f9620fcdcc2afde88efcc54cd38024c1c4b5fc8f
Bruno Campagnol <bruno_unky@hotmail.com>
2024-06-13 14:37:35 -0300

    Menu
    Atualização do menu com as opções de contribuição

--- App C++/main.cpp
+++ App C++/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <locale>
 #include <cstring>
+#include <stdio.h>
 
 using namespace std;
 
@@ -41,43 +42,67 @@ void cadastroAluno() {
 	cout << "!!Aluno novo foi cadastrado. Use novamente as opções do menu!!" << endl;
 }
 
-void mostrarAlunosCadastrados() {
+void mostrarAlunos() {
     if (contAluno == 0) {
         cout << "Nenhum aluno cadastrado." << endl;
         return;
     }
 
-    cout << "Lista de Alunos Cadastrados:" << endl;
-    cout << "-----------------------------" << endl;
+    cout << "________ Lista de Alunos ________" << endl;
 
     for (int i = 0; i < contAluno; ++i) {
         cout << "ID: " << newAluno[i].id << endl;
         cout << "Nome: " << newAluno[i].nome << endl;
-        cout << "Semestre de Início: " << newAluno[i].semestreInicio << endl;
-        cout << "Ano de Início: " << newAluno[i].anoInicio << endl;
+        cout << "Semestre de Inicio: " << newAluno[i].semestreInicio << endl;
+        cout << "Ano de Inicio: " << newAluno[i].anoInicio << endl;
         cout << "Curso: " << newAluno[i].curso << endl;
-        cout << "-----------------------------" << endl;
     }
 }
 
 void exportarTXTalunos() {
-    cout << "Função de exportação de alunos para TXT ainda não implementada." << endl;
+    FILE* outFile = fopen("alunosCadastrados.txt", "w"); //nome do arquivo que sera criado
+
+    if (!outFile) {
+        cout << "Erro ao abrir o arquivo de saída." << endl;
+        return;
+    }
+
+    fprintf(outFile, "Lista de Alunos Cadastrados:\n");
+    fprintf(outFile, ".\n");
+    fprintf(outFile, ".:\n");
+
+    for (int i = 0; i < contAluno; ++i) {
+        fprintf(outFile, "ID: %d\n", newAluno[i].id);
+        fprintf(outFile, "Nome: %s\n", newAluno[i].nome.c_str());
+        fprintf(outFile, "Semestre de Início: %d\n", newAluno[i].semestreInicio);
+        fprintf(outFile, "Ano de Início: %d\n", newAluno[i].anoInicio);
+        fprintf(outFile, "Curso: %s\n", newAluno[i].curso.c_str());
+        fprintf(outFile, "-----------------------------\n");
+    }
+
+    fclose(outFile);
+    cout << "Dados dos alunos exportados para 'alunosCadastrados.txt' com sucesso!!!." << endl;
 }
+
 
 void menu() {
     int opMenu;
 
     do {
-        cout << "___________ Turma do Cafe - Fatec Araras ___________" << endl;
+        cout << "________ Turma do Cafe - Fatec Araras ________" << endl;
         cout << endl;
-        cout << "Digite a opcao desejada: " << endl;
+        cout << "Digite a opcao desejada: " << endl << endl;;
         cout << "___________________ ALUNOS ___________________" << endl;
         cout << "1 - Inserir um novo aluno" << endl;
         cout << "2 - Editar dados de um contribuente já cadastrado" << endl;
         cout << "3 - Importar TXT com os dados dos alunos" << endl;
         cout << "4 - Exportar TXT com os dados dos alunos" << endl;
+        cout << "5 - Mostrar todos os alunos cadastrados" << endl << endl; // Opção adicionada para mostrar alunos
         cout << "________________ CONTRIBUENTES _______________" << endl;
-        cout << "5 - Mostrar todos os alunos cadastrados" << endl; // Opção adicionada para mostrar alunos
+        cout << "6 - Inserir uma nova comtribuição" << endl;
+        cout << "7 - Exportar TXT com os dados dos contribuentes" << endl;
+        cout << "8 - Exportar TXT com os dados dos contribuentes pelo curso do aluno" << endl;
+        cout << "9 - Mostrar todas as contribuições cadastradas" << endl;
         cout << "0 - Para finalizar o programa" << endl << endl;
         cin >> opMenu;
 		
@@ -89,7 +114,7 @@ void menu() {
                 exportarTXTalunos();
                 break;
             case 5:
-                mostrarAlunosCadastrados();
+                mostrarAlunos();
                 break;
             case 0:
                 cout << "Programa finalizado" << endl;
@@ -104,6 +129,9 @@ void menu() {
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
+    
+    cout << "DEV Bruno Campagnol de Oliveira" << endl;
+    
     menu();
     delete[] newAluno; //fazer teste depois para ver se esta limpa a memoria
     return 0;
