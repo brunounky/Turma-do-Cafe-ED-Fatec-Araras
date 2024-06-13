cdf2ab2a28f8ee0a23f189dc0af50a30e829b955
Bruno Campagnol <bruno_unky@hotmail.com>
2024-06-13 09:31:51 -0300

    Att
    Funcionou a gravação no ponteiro, porem o ID esta comecando com 0

--- App C++/main.cpp
+++ App C++/main.cpp
@@ -1,102 +1,111 @@
-// Projeto desenvolvido por Bruno Campagnol de Oliveira
-// Estrutura de Dados - Orlando - Fatec Araras 2024
-
 #include <iostream>
-#include <stdlib.h>
 #include <locale>
 #include <cstring>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 using namespace std;
 
-int idAtt = 1; //Inicializa o app com o id com esse valor. Tenho que testar se é melhor usar 0 ou 1
 
-struct aluno {
+struct aluno { //dados aluno
     int id;
     string nome;
     int semestreInicio;
     int anoInicio;
     string curso;
-    int *ptrAluno;
 };
 
-struct contribuinte {
-    int id;
-    int mes;
-    int ano;
-    float valor;
-    int *ptrContribuente;
-};
+aluno* newAluno = 0; // ponteiro 
+int contAluno = 0; // inicia o contador como zero
 
+void cadastroAluno() {
 
-void cadastroAluno(){ // Opção 1 do MENU principal
-	
-	aluno newAluno;
-	newAluno.id = idAtt++; //Soma mais um ao id
+    aluno* temp = new aluno[contAluno + 1]; //array temporario
+		
+	cout << "ID do novo aluno: " << contAluno << endl;
 	cout << "Informe o primeiro nome do Aluno: " << endl;
-	cin >> newAluno.nome;
+	cin >> temp[contAluno].nome;
 	cout << "Informe o semestre de inicio na Fatec: " << endl;
-	cin >> newAluno.semestreInicio;
+	cin >> temp[contAluno].semestreInicio;
 	cout << "Informe o ano de inicio na Fatec: " << endl;
-	cin >> newAluno.anoInicio;
+	cin >> temp[contAluno].anoInicio;
 	cout << "Informe o curso da Fatec (DSM ; SI ; GE): " << endl;
-	cin >> newAluno.curso;
+	cin >> temp[contAluno].curso;
 	
-	
+    for (int i = 0; i < contAluno; ++i) { //gravação do array temp no principal
+        temp[i] = newAluno[i];
+    }
+    
+    delete[] newAluno; // Liberando a memória do array antigo
+    newAluno = temp;   // Atualizando para apontar para o novo array
+    contAluno++;       // Incrementando o contador de alunos
+
 	cout << "!!Aluno novo foi cadastrado. Use novamente as opções do menu!!" << endl;
-	
 }
 
-void exportarTXTalunos(){ // Opção 4 no MENU
-	
-	cout << "Teste para ver se vem do menu pra ca" << endl;
+void mostrarAlunosCadastrados() {
+    if (contAluno == 0) {
+        cout << "Nenhum aluno cadastrado." << endl;
+        return;
+    }
+
+    cout << "Lista de Alunos Cadastrados:" << endl;
+    cout << "-----------------------------" << endl;
+
+    for (int i = 0; i < contAluno; ++i) {
+        cout << "ID: " << newAluno[i].id << endl;
+        cout << "Nome: " << newAluno[i].nome << endl;
+        cout << "Semestre de Início: " << newAluno[i].semestreInicio << endl;
+        cout << "Ano de Início: " << newAluno[i].anoInicio << endl;
+        cout << "Curso: " << newAluno[i].curso << endl;
+        cout << "-----------------------------" << endl;
+    }
 }
 
-//Inicio do programa tem que vir aqui
-void menu(){
+void exportarTXTalunos() {
+    cout << "Função de exportação de alunos para TXT ainda não implementada." << endl;
+}
+
+void menu() {
+    int opMenu;
+
+    do {
+        cout << "___________ Turma do Cafe - Fatec Araras ___________" << endl;
+        cout << endl;
+        cout << "Digite a opcao desejada: " << endl;
+        cout << "___________________ ALUNOS ___________________" << endl;
+        cout << "1 - Inserir um novo aluno" << endl;
+        cout << "2 - Editar dados de um contribuente já cadastrado" << endl;
+        cout << "3 - Importar TXT com os dados dos alunos" << endl;
+        cout << "4 - Exportar TXT com os dados dos alunos" << endl;
+        cout << "________________ CONTRIBUENTES _______________" << endl;
+        cout << "5 - Mostrar todos os alunos cadastrados" << endl; // Opção adicionada para mostrar alunos
+        cout << "0 - Para finalizar o programa" << endl << endl;
+        cin >> opMenu;
 		
-	int opMenu;
-	
-	do{
-		cout << "___________ Turma do Cafe - Fatec Araras ___________" << endl;
-		cout << endl;
-		cout << "Digite a opcao desejada: " << endl;
-		cout << "___________________ ALUNOS ___________________" << endl;
-		cout << "1 - Inserir um novo aluno" << endl;
-		cout << "2 - Editar dados de um contribuente já cadastrado" << endl;
-		cout << "3 - Importar TXT com os dados dos alunos" << endl;
-		cout << "4 - Exportar TXT com os dados dos alunos" << endl;
-		cout << "________________ CONTRIBUENTES _______________" << endl;
-		cout << "4 - Cadastrar contribuicao" << endl;
-		cout << "0 - Para finalizar o programa" << endl << endl;
-		cin >> opMenu;
+        switch(opMenu) {
+            case 1:
+                cadastroAluno();
+                break;
+            case 4:
+                exportarTXTalunos();
+                break;
+            case 5:
+                mostrarAlunosCadastrados();
+                break;
+            case 0:
+                cout << "Programa finalizado" << endl;
+                break;
+            default:
+                cout << "Informe uma opção válida dentre as possibilidades" << endl;
+                break;
+        }
 		
-		switch(opMenu){
-			case 1:
-				cadastroAluno();
-				break;
-			case 4:
-				exportarTXTalunos();
-			case 0:
-				cout << "Programa finalizado" << endl;;
-				break;
-			default:
-				cout << "Informe uma opção válida dentre as possibilidades" << endl;
-				break;
-	}
-		
-	}while (opMenu != 0);
-	
+    } while (opMenu != 0);
 }
 
-
-
-int main(int argc, char** argv) {
-	
-	setlocale (LC_ALL, "Portuguese");
-	
-	menu();
-
-	return 0;
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    menu();
+    delete[] newAluno; //fazer teste depois para ver se esta limpa a memoria
+    return 0;
 }
+
